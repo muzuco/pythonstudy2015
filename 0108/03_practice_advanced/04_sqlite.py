60aa9cef11c073cc6ebc5e63dc1e7590bbb625f3
muzuco <muzuco@netmarble.com>
2015-01-08 17:25:38 +0900

    Edit sqlite.py

--- 0108/03_practice_advanced/04_sqlite.py
+++ 0108/03_practice_advanced/04_sqlite.py
@@ -1,5 +1,25 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+import smtplib
+
 from sqlalchemy import create_engine
 
-engine = create_engine('sqlite://',echo=True)
-connection = engine.connect()
-r1 = connection.execute('select domain_id, is_edited from check_records')
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    engine = create_engine('sqlite:///C:\\sqlite\\test.db', echo=True)
+    connection = engine.connect()
+    r = connection.execute('select * from first')
+    data=[]
+    for row in r:
+        data.append(row)
+        
+    return render_template('sqlite.html', data = data)   
+    #for row in r1:
+    #    print row
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0108/03_practice_advanced/99_test.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///C:\\sqlite\\test.db', echo=True)
+Base = declarative_base()
+
+
+class School(Base):
+
+    __tablename__ = "woot"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)  
+
+
+    def __init__(self, name):
+
+        self.name = name    
+
+Base.metadata.create_all(engine)
