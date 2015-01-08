35e1dbe6cdb4da9547e9d1c4f2be4ffb196b57f4
muzuco <muzuco@netmarble.com>
2015-01-08 15:53:04 +0900

    Edit send_email.py

--- 0108/03_practice_advanced/03_send_email.py
+++ 0108/03_practice_advanced/03_send_email.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
+import smtplib
+
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -9,10 +11,23 @@ def index():
     
 @app.route('/send_email', methods=['POST'])
 def send_email():
+    # login info
+    username = 'buskerlic'
+    password = 'rhrhtld1@g'
+    toaddrs = 'muzuco@netmarble.com'
+    fromaddrs = 'buskerlic@gmail.com'
     title = request.form['Title']
     contents = request.form['Contents']
-    
-    return title + contents
+    message = 'Subject: %s\n\n%s' % (title, contents)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com:587")
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(fromaddrs, toaddrs, message)
+        server.quit()
+        return 'OK: Check the mail.'
+    except smtplib.SMTPException:
+        return 'Error: unable to send email.'
 
 if __name__ == '__main__':
     app.run()
